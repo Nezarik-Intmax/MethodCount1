454f0e15368dabe6aa04c987241dbab8e4ff0cb1
Nezarik-Intmax <nezarikintmax@gmail.com>
2022-10-18 02:40:46 +0500

    almost

--- MethodCount1/MethodCount1.cpp
+++ MethodCount1/MethodCount1.cpp
@@ -7,9 +7,9 @@ double b = 0.3;
 double e = M_E;
 double c = 2.89;
 double d = 0.28;
+double eps = 0.001;
 
 double f(double x, double y){
-    std::cout << pow(e, c * x * x + d * y * y);
     return a*x + b*y + pow(e, c*x*x + d*y*y);
          //18*x1 + 0.3*x2 + e^(2.89*x1*x1 + 0.28*x2*x2)
 }
@@ -34,18 +34,16 @@ int main()
     double y = 0;
     double old_y = 0;
     double alpha = 1;
-    double min = f(x, y) + 1;
-    while(true){
+    double min = f(x, y);
+    while(sqrt(pow(ff_x(old_x, old_y), 2) + pow(ff_y(old_x, old_y), 2)) >= eps){
+        x = get_k_x(old_x, old_y, alpha);
+        y = get_k_y(old_x, old_y, alpha);
         double tmp = f(x, y);
         std::cout << "X: " << x << "; Y: " << y << "; F = " << tmp << "\n";
         if (tmp < min){
             old_x = x;
             old_y = y;
             min = tmp;
-            x = get_k_x(old_x, old_y, alpha);
-            y = get_k_y(old_x, old_y, alpha);
-        }else if(tmp == min){
-            break;
         }else{
             alpha /= 2;
             x = old_x;
